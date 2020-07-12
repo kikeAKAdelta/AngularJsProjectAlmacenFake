dc5d84898fda46b147322cc4b7c37128c80e2f8d
kikeAKAdelta <kike04arevalo@gmail.com>
2020-07-12 17:01:36 -0600

    rest empleados

--- almacenApp.js
+++ almacenApp.js
@@ -20,8 +20,12 @@ appAlmacen.controller("controllerAlmacen", function($scope, $http){
 
     
         $http.get("https://jsonfy.com/items").then(function(response){
-            console.log(response.data);
             $scope.producto = response.data;
+        });
+
+        $http.get("http://dummy.restapiexample.com/api/v1/employees").then(function(response){
+            console.log(response.data.data);
+            $scope.empleado = response.data.data;
         });
     
 });
